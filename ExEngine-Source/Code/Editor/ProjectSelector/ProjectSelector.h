1c5c4b171399620b546e595fe9e937ebda93eff0
AlanSilva <alanfs1010@gmail.com>
2025-11-25 10:16:52 -0300

    Build as .app

--- ExEngine-Source/Code/Editor/ProjectSelector/ProjectSelector.h
+++ ExEngine-Source/Code/Editor/ProjectSelector/ProjectSelector.h
@@ -1,10 +1,11 @@
 #pragma once 
 #include "ProjectSelectorInfo.h"
+#include "../../Engine/Core/Engine.h"
 #include <string>
 #include <SDL2/SDL.h>
 
 #ifndef PROJECT_SELECTOR_PATH
-#define PROJECT_SELECTOR_PATH "Library/ProjectSelector/"
+#define PROJECT_SELECTOR_PATH Engine::GetEnginePath()/"Library/ProjectSelector/"
 #endif
 
 class ProjectSelector{
